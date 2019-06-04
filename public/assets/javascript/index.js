a723bb1385cb95aa319c5e15f28e1e04eeeef2fd
Jessica Byrd <byrdjess@me.com>
2019-06-04 00:39:04 -0700

    Completed routes and started js

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+var articleContainer = $(".article-container");
+$(document).on("click", ".btn.save", handleArticleSave);
+$(document).on("click", ",scrape-new", handleArticleScrape)
+
+initPage();
+
+function initPage() {
+
+    articleContainer.empty();
+    $.get("api/headlines?saved=false")
+    .then(function(data){
+        if (data && data.length) {
+            renderArticles(data);
+        }
+        else {
+            renderEmpty();
+        }
+    });
+}
+
+function renderArticles(articles) {
+
+    var articlePanels = [];
+
+    for (var i = 0; i < articles.length; i++) {
+        articlePanels.push(createPanel(articles[i]));
+    }
+}
+
+
+
+
+
+
+
+
+});
